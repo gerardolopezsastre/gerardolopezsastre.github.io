6b8eecf9792501c9424474737288969146282129
gerardolopezsastre <lopezsastregerardo@gmail.com>
2025-10-14 08:41:39 +0000

    Deploying to gh-pages from @ gerardolopezsastre/gerardolopezsastre.github.io@c9c805ac72e9b40f399180403df0d5bc0cc7dab6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,6 +48,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/blog-JORGE/";
           
         },
+      },{id: "post-blog-prueba",
+        
+          title: "Blog Prueba",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/blog-prueba/";
+          
+        },
       },{id: "projects-hume-investigación-sobre-los-principios-de-la-moral-trad",
           title: 'Hume: Investigación sobre los principios de la moral (trad.)',
           description: "Espasa Calpe",
